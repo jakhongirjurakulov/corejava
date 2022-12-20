113f58e1a013c9cfed635708021d183e112f9b51
jakhongirjurakulov <65897453+jakhongirjurakulov@users.noreply.github.com>
2022-12-20 17:48:36 +0500

    Add files via upload

---
+++ 20.12.22/Inheritance 2nd part/Main.java
@@ -0,0 +1,59 @@
+public class Main {
+    public static void main(String[] args) {
+//        Upcasting and Downcasting
+//        Upcasting - Casting an object to one of its super types.
+//        Downcasting - casting an object to one of its sub types.
+
+//        var control = new UIControl(true);
+//        var textBox = new TextBox();
+
+//        Upcasting
+//        show(textBox);
+
+//        this will throw exception
+//        show(control);
+
+//        Comparing Objects
+
+        //var point1 = new Point(1, 2);
+        //var point2 = new Point(1, 2);
+
+        //System.out.println(point1 == point2);
+//        false - because comparing is in terms of address
+        //System.out.println(point1.equals(point2));
+//        false - because comparing is in terms of address
+
+//        var point1 = new Point(1, 2);
+//        var point2 = new Point(1, 2);
+
+        //System.out.println(point1.equals(new TextBox()));
+        //System.out.println(point1.equals(point1));
+//        System.out.println(point1.hashCode());
+//        System.out.println(point2.hashCode());
+
+//        Polymorphism - Many Forms. This allows objects to take many forms.
+
+//        UIControl[] controls = {new TextBox(), new CheckBox()};
+//        for (var control : controls)
+//            control.render();
+
+//        Abstract classes and methods - we use abstract classes in situations where we declare a class but we don't wanna be able to instantiate it (new instance of the class).
+//        Abstract method forces subclasses to implement this method.
+
+//        UIControl[] controls = {new UIControl(), new TextBox(), new CheckBox()};
+
+//        Final classes and Methods - Final classes cannot be extended (extends) rather than abstract classes.
+//        Final methods cannot be overrided
+
+//        AVOID Deep Inheritance Hierarchies (2 levels or no more than 3 levels)
+
+//        Multiple Inheritance - You aren't gonna need it (YAGNI)
+    }
+
+//    public static void show(UIControl control) {
+////        Downcasting
+//        var textBox = (TextBox)control;
+//        textBox.setText("Hello World");
+//        System.out.println(control);
+//    }
+}
---
+++ 20.12.22/Inheritance 2nd part/Point.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+
+    //    @Override
+//    public int hashCode() {
+//        return Objects.hash(x, y);
+//    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj)
+//            return true;
+//
+//        if (!(obj instanceof Point))
+//            return false;
+//
+//        var point = (Point) obj;
+//        return this.x == point.x && this.y == point.y;
+//    }
+}
