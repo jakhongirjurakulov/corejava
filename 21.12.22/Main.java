00373730e21cf9c18f8c3ad3e4b9e7051200b099
jakhongirjurakulov <65897453+jakhongirjurakulov@users.noreply.github.com>
2022-12-21 17:18:42 +0500

    Add files via upload

---
+++ 21.12.22/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+//        Interfaces - we use interfaces to build loosely-coupled, extensible, testable apps. (Abstraction - hide the implementation details).
+//        Interface - what should be done. Classes - how it should be done.
+
+//        Tightly-coupled Code
+
+//        Creating an Interface
+
+//        Dependency Injection (Our Classes should not instantiate their dependencies)
+
+//        Constructor injection
+
+//        var calculator = new TaxCalculator2018(100_000);
+//        var report = new TaxReport(calculator);
+
+//        Setter Injection
+
+//        var calculator = new TaxCalculator2018(100_000);
+//        var report = new TaxReport(calculator);
+//        report.show();
+//
+//        report.setCalculator(new TaxCalculator2019());
+//        report.show();
+
+//        Method Injection
+
+//        var calculator = new TaxCalculator2018(100_000);
+//        var report = new TaxReport();
+//        report.show(calculator);
+//        report.show(new TaxCalculator2019());
+
+//        Interface Segregation Principle - Divide big interfaces into smaller ones
+
+
+    }
+}
